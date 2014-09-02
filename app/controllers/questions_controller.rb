535bdac5f0e9c2071f4dbcfdb39bc54d6d7e3670
Jeff Stringer & mark rubenstein <student@epicodus.com>
2014-09-02 10:46:06 -0700

    add new/create for question model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,4 +2,24 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.all
   end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.new(question_params)
+    if @question.save
+      flash[:notice] = "#{@question.title} was added. Thanks for asking!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+private
+    def question_params
+      params.require(:question).permit(:title, :question)
+    end
+
 end
