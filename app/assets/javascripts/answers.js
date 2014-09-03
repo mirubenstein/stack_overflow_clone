918e8ebefc3eb409a4699b36aaaccdd917adddec
Jeff Stringer & mark rubenstein <student@epicodus.com>
2014-09-03 11:00:49 -0700

    add hiding answer form

---
+++ app/assets/javascripts/answers.js
@@ -0,0 +1,14 @@
+// $(function() {
+//   $('.edit_answer').submitOnCheck();
+// });
+
+// CoffeeScript Version:
+//
+// jQuery.fn.submitOnCheck = ->
+//   @find('input[type=submit]').remove()
+//   @find('input[type=checkbox]').click ->
+//     $(this).parent('form').submit()
+//   this
+//
+// jQuery ->
+//   $('.edit_task').submitOnCheck()
