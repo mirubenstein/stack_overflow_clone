b118ee80124bb54d801d3faa28bf532d074a996a
Jeff Stringer & mark rubenstein <student@epicodus.com>
2014-09-04 15:19:34 -0700

    work in progress working for answer question link, create answer form and remove answer

--- spec/features/question_show_spec.rb
+++ spec/features/question_show_spec.rb
@@ -11,4 +11,18 @@ describe "Questions" do
     click_link "Answer Question"
     expect(page).to have_button "Create Answer"
   end
+
+    it "creates answer when user clicks 'creates answer' button" , js: true do
+      visit '/login'
+      user = User.create(:email => 'user@example.com', :password => 'password')
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+      click_button 'Log in'
+      visit new_question_path
+      question = user.questions.create(title: 'test title', question: 'test question')
+      visit "/questions/#{question.id}"
+      click_link "Answer Question"
+      fill_in 'Answer', :with => ''
+
+    end
 end
