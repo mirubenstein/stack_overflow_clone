1eb516d21b880b27c6a8940cefdbf590dd1830fa
Jeff Stringer & mark rubenstein <student@epicodus.com>
2014-09-04 15:29:32 -0700

    add capybara spec to test for answer removal

--- spec/features/question_show_spec.rb
+++ spec/features/question_show_spec.rb
@@ -26,4 +26,20 @@ describe "Questions" do
       click_button "Create Answer"
       expect(page).to have_content "test answer"
     end
+
+    it "removes an answer correctly entered by user" , js: true do
+      visit '/login'
+      user = User.create(:email => 'user@example.com', :password => 'password')
+      fill_in 'Email', :with => 'user@example.com'
+      fill_in 'Password', :with => 'password'
+      click_button 'Log in'
+      visit new_question_path
+      question = user.questions.create(title: 'test title', question: 'test question')
+      visit "/questions/#{question.id}"
+      click_link "Answer Question"
+      fill_in 'Answer', :with => 'test answer'
+      click_button "Create Answer"
+      click_link "(remove)"
+      expect(page).to_not have_content "test answer"
+    end
 end
